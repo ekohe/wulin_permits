d8068309a474547b5af61a026e7825a71ab7324d
Xuhao <xuhao@rubyfans.com>
2012-04-26 16:57:51 +0800

    MISC - Implement admin role configuration

--- lib/wulin_permits.rb
+++ lib/wulin_permits.rb
@@ -1,7 +1,31 @@
 require "wulin_permits/version"
 require 'wulin_permits/engine' if defined?(Rails)
 
-module WulinPermits; end
+module WulinPermits
+  # Define admin email for your app with +WulinPermits.admin=+
+  # Create a file localed config/initializers/wulin_permits.rb
+  # and define admin like:
+  #
+  # config/initializers/wulin_permits.rb:
+  #
+  #   WulinPermits.admin = 'admin@example.com'
+  # 
+  # get defined roles like:
+  # 
+  #   WulinPermits.admin    #=>   admin@example.com
+  #
+  
+  def admin
+    @@admin ||= ''
+  end
+  
+  def admin=(email)
+    raise 'Roles must be an array instance' unless String === email
+    @@admin = email
+  end
+  
+  module_function :admin, :admin=
+end
 
 require 'wulin_permits/helper'
 require 'wulin_permits/screen_controller_extension'
--- lib/wulin_permits/engine.rb
+++ lib/wulin_permits/engine.rb
@@ -5,9 +5,6 @@ module WulinPermits
     initializer "include extensions to screen controller" do |app|
       WulinMaster::ScreenController.send :include, WulinPermits::ScreenControllerExtension
       User.send(:include, WulinPermits::Helper)
-      
-      # Override the existing +admin?+ method
-      User.send(:undef_method, :admin?)
     end
   end
 end
