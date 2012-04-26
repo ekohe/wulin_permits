73bdeb4b3805dbf0da096d334198407aa1e5759a
Xuhao <xuhao@rubyfans.com>
2012-04-26 12:41:00 +0800

    MISC - Improvements

--- lib/wulin_permits/engine.rb
+++ lib/wulin_permits/engine.rb
@@ -5,6 +5,9 @@ module WulinPermits
     initializer "include extensions to screen controller" do |app|
       WulinMaster::ScreenController.send :include, WulinPermits::ScreenControllerExtension
       User.send(:include, WulinPermits::Helper)
+      
+      # Override the existing +admin?+ method
+      User.send(:undef_method, :admin?)
     end
   end
 end
--- lib/wulin_permits/helper.rb
+++ lib/wulin_permits/helper.rb
@@ -1,14 +1,22 @@
 module WulinPermits
   module Helper
     extend ActiveSupport::Concern
+  
+    included do |base|
+      extend ActiveSupport::Memoizable
+    end
     
     def method_missing(method_name, *args, &block)
       if method_name =~ /(.*)\?$/ and (role = Role.find_by_name($1)) and args.blank?
-        UserRole.where(user_id: self.id).map(&:role).include? role.id
+        self.class.send(:define_method, method_name, proc { UserRole.where(user_id: self.id).map(&:role).include?(role.id) } )
+        
+        self.class.memoize method_name.to_sym # Cache it
       else
         super(method_name, *args, &block)
       end
     end
+    
+    
 
   end
 end
