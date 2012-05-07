c56bc5c0b059c1200380ca5ef990fab427ec639f
Xuhao <xuhao@rubyfans.com>
2012-05-07 17:28:29 +0800

    REFACTOR - Remove ActiveSupport::Memoizable and other improvements

--- lib/wulin_permits/extensions/screen.rb
+++ lib/wulin_permits/extensions/screen.rb
@@ -4,22 +4,24 @@ module WulinPermits
       extend ActiveSupport::Concern
       
       included do |base|
-        extend ActiveSupport::Memoizable
-
         class_eval do
           def authorized?(user)
-            permission = Permission.find_or_create_by_name("#{name}#read")
-            user.has_permission?(permission)
+            @authorized ||= {}
+            @authorized[user] ||= begin
+              permission = Permission.find_or_create_by_name("#{name}#read")
+              user.has_permission?(permission)
+            end
           end
 
           def authorize_create?(user)
-            permission = Permission.find_or_create_by_name("#{name}#cud")
-            user.has_permission?(permission)
+            @authorize_create ||= {}
+            @authorize_create[user] ||= begin
+              permission = Permission.find_or_create_by_name("#{name}#cud")
+              user.has_permission?(permission)
+            end
           end
           alias_method :authorize_update?, :authorize_create?
           alias_method :authorize_destroy?, :authorize_create?
-          
-          memoize :authorized?, :authorize_create?
         end
 
       end
--- lib/wulin_permits/extensions/user.rb
+++ lib/wulin_permits/extensions/user.rb
@@ -4,26 +4,26 @@ module WulinPermits
       extend ActiveSupport::Concern
 
       included do |base|
-        extend ActiveSupport::Memoizable
 
         class_eval do
           # Override the existing +admin?+ method
           def admin?
-            return true if self.email == WulinPermits.admin
-
-            role = Role.find_by_name('admin')
-            UserRole.where(user_id: self.id).pluck(:role).uniq.include?(role.id)
+            @admin ||= has_permission_with_name?('admin')
           end
           
           def has_permission?(permission)
-            return true if self.email == WulinPermits.admin
-            role_ids = UserRole.where(user_id: self.id).pluck(:role).uniq
-            PermissionsRole.where('role_id in (?)', role_ids).pluck(:permission_id).include? (permission.id)
+            @has_permission ||= {}
+            @has_permission[permission] ||= begin
+              return true if self.email == WulinPermits.admin
+              !permission.user_roles.where(user_id: self.id).count.zero?
+            end
           end
-          memoize :admin?, :has_permission?
           
           def has_permission_with_name?(permission_name)
-            has_permission?(Permission.find_or_create_by_name(permission_name))
+            @has_permission_with_name ||= {}
+            @has_permission_with_name[permission_name] ||= begin
+              has_permission?(Permission.find_or_create_by_name(permission_name))
+            end
           end
         end
 
@@ -32,8 +32,6 @@ module WulinPermits
       def method_missing(method_name, *args, &block)
         if method_name =~ /(.*)\?$/ and (role = Role.find_by_name($1)) and args.blank?
           self.class.send(:define_method, method_name, proc { UserRole.where(user_id: self.id).pluck(:role).uniq.include?(role.id) } )
-
-          self.class.memoize method_name.to_sym # Cache it
         else
           super(method_name, *args, &block)
         end
