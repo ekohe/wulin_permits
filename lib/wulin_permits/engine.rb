1c7c640f0c336eb63f362e87f9b8cd440b08a4ff
Jimmy Huang <pake007@gmail.com>
2012-05-10 15:50:49 +0800

    MISC - fixed the role_screen css precompile config

--- lib/wulin_permits/engine.rb
+++ lib/wulin_permits/engine.rb
@@ -15,6 +15,7 @@ module WulinPermits
       require 'wulin_permits/wulin_master/user_screen' unless defined? UserScreen
       require 'wulin_permits/wulin_master/users_controller' unless defined? UsersController
       WulinMaster::add_stylesheet 'role_screen.css' unless WulinMaster::stylesheets.include? 'role_screen.css'
+      app.config.assets.precompile += %w( role_screen.css )
     end
   end
 end
