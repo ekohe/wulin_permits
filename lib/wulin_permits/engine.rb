461b9b2f71afbb3dacc3ca73879b311fdbc56045
Jimmy Huang <pake007@gmail.com>
2012-08-27 17:00:58 +0800

    FEATURE #11438 1h - Use carousel for users / roles
    - fixed some file position problem

--- lib/wulin_permits/engine.rb
+++ lib/wulin_permits/engine.rb
@@ -11,9 +11,6 @@ module WulinPermits
       WulinMaster::ScreenController.send :include, WulinPermits::Extensions::ScreenController
       WulinMaster::Screen.send :include, WulinPermits::Extensions::Screen
       
-      require 'wulin_permits/wulin_master/user_grid' unless defined? UserGrid
-      require 'wulin_permits/wulin_master/user_screen' unless defined? UserScreen
-      require 'wulin_permits/wulin_master/users_controller' unless defined? UsersController
       WulinMaster::add_stylesheet 'role_screen.css' unless WulinMaster::stylesheets.include? 'role_screen.css'
       WulinMaster::add_javascript 'wulin_permits.js' unless WulinMaster::javascripts.include? 'wulin_permits.js'
       app.config.assets.precompile += %w(role_screen.css wulin_permits.js)
