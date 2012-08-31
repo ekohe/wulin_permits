8f51d00e68d390bb3ec967c9bf7f90da0e462948
Jimmy Huang <pake007@gmail.com>
2012-08-31 12:07:43 +0800

    MISC - change add remove icons

--- app/grids/roles_user_grid.rb
+++ app/grids/roles_user_grid.rb
@@ -7,12 +7,12 @@ class RolesUserGrid < WulinMaster::Grid
   column :user, option_text_attribute: 'email', only: [:MasterRoleDetailUserScreen]
   
   # actions for master sad detail role grid
-  action :add_detail, model: 'role', screen: 'AddRoleForUserScreen', title: 'Add Roles', icon: 'add', only: [:MasterUserDetailRoleScreen]
-  action :delete, title: 'Remove Roles', only: [:MasterUserDetailRoleScreen]
+  action :add_detail, model: 'role', screen: 'AddRoleForUserScreen', title: 'Add Roles', icon: 'link', only: [:MasterUserDetailRoleScreen]
+  action :delete, title: 'Remove Roles', icon: 'unlink', only: [:MasterUserDetailRoleScreen]
 
   # actions for master service detail sad grid
-  action :add_detail, model: 'user', screen: 'AddUserForRoleScreen', title: 'Add Users', icon: 'add', only: [:MasterRoleDetailUserScreen]
-  action :delete, title: 'Remove Users', only: [:MasterRoleDetailUserScreen]
+  action :add_detail, model: 'user', screen: 'AddUserForRoleScreen', title: 'Add Users', icon: 'link', only: [:MasterRoleDetailUserScreen]
+  action :delete, title: 'Remove Users', icon: 'unlink', only: [:MasterRoleDetailUserScreen]
   
   # Common actions
   action :filter
