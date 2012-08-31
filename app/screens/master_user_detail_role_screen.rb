82b553d6233b130678ca6e6b65f19543dd9a7ede
Xuhao <xuhao@rubyfans.com>
2012-08-31 16:00:41 +0800

    MISC - UserRole screen corrections

--- app/screens/master_user_detail_role_screen.rb
+++ app/screens/master_user_detail_role_screen.rb
@@ -7,11 +7,3 @@ class MasterUserDetailRoleScreen < WulinMaster::Screen
   panel CarouselSwitchPanel, width: '6%', height: '100%', css: 'position: absolute; bottom: 0; left: 47%;', next_screen: 'MasterRoleDetailUserScreen'
   grid RolesUserGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; right: 0;', title: 'Roles with User', master_grid: 'UserGrid', eager_loading: false
 end
-
-class MasterRoleDetailUserScreen < WulinMaster::Screen
-  path '/roles'
-  
-  grid RoleGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; left: 0;'
-  panel CarouselSwitchPanel, width: '6%', height: '100%', css: 'position: absolute; bottom: 0; left: 47%;', next_screen: 'MasterUserDetailRoleScreen'
-  grid RolesUserGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; right: 0;', title: 'Users for Role', master_grid: 'RoleGrid', eager_loading: false
-end
