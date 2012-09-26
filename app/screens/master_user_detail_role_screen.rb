0bfe246e321841f7dcf5aa3caf3d9e3822eb90af
Maxime Guilbot <maxime@ekohe.com>
2012-09-26 17:09:39 +0800

    BUG #12075 - User Roles screen shouldn't be available in the menu for non-mima admins

--- app/screens/master_user_detail_role_screen.rb
+++ app/screens/master_user_detail_role_screen.rb
@@ -6,4 +6,8 @@ class MasterUserDetailRoleScreen < WulinMaster::Screen
   grid UserGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; left: 0;'
   panel CarouselSwitchPanel, width: '6%', height: '100%', css: 'position: absolute; bottom: 0; left: 47%;', next_screen: 'MasterRoleDetailUserScreen'
   grid RolesUserGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; right: 0;', title: 'Roles with User', master_grid: 'UserGrid', eager_loading: false
+
+  def authorized?(user)
+    user.admin?
+  end
 end
