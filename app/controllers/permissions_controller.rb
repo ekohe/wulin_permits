4405ee0d4d0b1428db7ae4df791ec3dc817c7a14
Jimmy Huang <pake007@gmail.com>
2012-10-09 12:32:31 +0800

    MISC - ignore roles params when updating permission

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,2 +1,7 @@
 class PermissionsController < WulinMaster::ScreenController
+  before_filter :delete_roles_param, only: :update
+
+  def delete_roles_param
+    params[:item].delete(:roles)
+  end
 end
