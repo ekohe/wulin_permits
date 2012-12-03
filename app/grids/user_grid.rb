8851f3fc7c70f251bfa0741dfe29e7ba4993a447
Xuhao <xuhao@rubyfans.com>
2012-12-03 12:15:50 +0800

    MISC - Clean up user_grid.rb file

--- app/grids/user_grid.rb
+++ app/grids/user_grid.rb
@@ -2,16 +2,13 @@ class UserGrid < WulinMaster::Grid
   title 'Users'
 
   model User
-  
-  # fill_window
 
   multi_select false
   
-  cell_editable false   
+  cell_editable false
 
   column :email, :width => 500
   
-  # action :roles_user, title: "Role", icon: 'roles_user'
   action :filter
   action :excel
 end
