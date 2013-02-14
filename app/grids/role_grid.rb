350f703f68d8125ff006932d43203438d3b0d9dd
stephan <stephan@kosmogo.com>
2013-02-14 18:11:40 +0100

    add buttons in role
    Signed-off-by: stephan <stephan@kosmogo.com>

--- app/grids/role_grid.rb
+++ app/grids/role_grid.rb
@@ -8,4 +8,6 @@ class RoleGrid < WulinMaster::Grid
   load_default_actions({only: [:MasterRoleDetailUserScreen]})
 
   action :filter, only: [:AddRoleForUserScreen]
+  action :add 
+  action :delete
 end
