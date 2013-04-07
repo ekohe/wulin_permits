3229777f0089df7d1e0e466b22c7eeb2e7559021
Xuhao <xuhao@rubyfans.com>
2013-04-07 17:20:27 +0800

    MISC - migrate file fix

--- db/migrate/20120823103753_rename_to_roles_users_of_user_roles_table.rb
+++ db/migrate/20120823103753_rename_to_roles_users_of_user_roles_table.rb
@@ -1,9 +1,9 @@
 class RenameToRolesUsersOfUserRolesTable < ActiveRecord::Migration
   def up
-    rename_table :user_roles, :roles_users if table_exists?(:user_roles)
+    rename_table :user_roles, :roles_users if !table_exists?(:roles_users)
   end
 
   def down
-    rename_table :roles_users, :user_roles if table_exists?(:roles_users)
+    rename_table :roles_users, :user_roles if !table_exists?(:user_roles)
   end
 end
