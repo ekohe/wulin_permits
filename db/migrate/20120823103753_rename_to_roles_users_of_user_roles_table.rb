a5fe7f6434cb9ab3e19e74e4961391ec96cd2c0e
Xuhao <xuhao@rubyfans.com>
2013-04-08 17:47:04 +0800

    MISC - rename user_roles migrate bugfix

--- db/migrate/20120823103753_rename_to_roles_users_of_user_roles_table.rb
+++ db/migrate/20120823103753_rename_to_roles_users_of_user_roles_table.rb
@@ -1,9 +1,9 @@
 class RenameToRolesUsersOfUserRolesTable < ActiveRecord::Migration
   def up
-    rename_table :user_roles, :roles_users if !table_exists?(:roles_users)
+    rename_table :user_roles, :roles_users if !table_exists?(:roles_users) and !const_defined?('TerranovaCore')
   end
 
   def down
-    rename_table :roles_users, :user_roles if !table_exists?(:user_roles)
+    rename_table :roles_users, :user_roles if !table_exists?(:user_roles) and !const_defined?('TerranovaCore')
   end
 end
