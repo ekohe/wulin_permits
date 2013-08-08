caf2e8f3a8879ab23ac5f6240421c70f3488f59b
Xuhao <xuhao@rubyfans.com>
2013-08-08 16:31:51 +0800

    BUG - Memoization bugfix

--- lib/wulin_permits/extensions/screen.rb
+++ lib/wulin_permits/extensions/screen.rb
@@ -2,35 +2,35 @@ module WulinPermits
   module Extensions
     module Screen
       extend ActiveSupport::Concern
-      
+
       included do
-        class_eval do
-
-          def authorized?(user)
-            @authorized ||= {}
-            @authorized[user] ||= authorized_for_user_and_permission?(user, "#{name}#read")
-          end
-
-          def authorize_create?(user)
-            @authorize_create ||= {}
-            @authorize_create[user.email] ||= authorized_for_user_and_permission?(user, "#{name}#cud")
-          end
-
-          alias_method :authorize_update?, :authorize_create?
-          alias_method :authorize_destroy?, :authorize_create?
-
-          private
-            def authorized_for_user_and_permission?(user, permission)
-              if user.respond_to?(:admin?) && user.admin?
-                true
-              else
-                user.has_permission?(Permission.find_or_create_by_name(permission))
-              end
-            end
+        def authorized?(user=nil)
+          user ||= current_user
+          @authorized ||= {}
+          return @authorized[user.email] if @authorized.key?(user.email)
+          @authorized[user.email] = authorized_for_user_and_permission?(user, "#{name}#read")
         end
 
+        def authorize_create?(user=nil)
+          user ||= current_user
+          @authorize_create ||= {}
+          return @authorize_create[user.email] if @authorize_create.key?(user.email)
+          @authorize_create[user.email] = authorized_for_user_and_permission?(user, "#{name}#cud")
+        end
+
+        alias_method :authorize_update?, :authorize_create?
+        alias_method :authorize_destroy?, :authorize_create?
+
+        private
+          def authorized_for_user_and_permission?(user, permission)
+            if user.respond_to?(:admin?) && user.admin?
+              true
+            else
+              user.has_permission?(Permission.find_or_create_by_name(permission))
+            end
+          end
+
       end
-      
     end
   end
 end
--- lib/wulin_permits/extensions/user.rb
+++ lib/wulin_permits/extensions/user.rb
@@ -3,36 +3,26 @@ module WulinPermits
     module User
       extend ActiveSupport::Concern
 
-      included do |base|
-
-        class_eval do
-          # Override the existing +admin?+ method
-          # def admin?
-          #   @admin ||= begin
-          #     # return true if self.respond_to?(:admin?) && self.admin?
-          # 
-          #     role = Role.where("UPPER(services.code) = (?)", 'admin').first #Role.find_by_name('admin')
-          #     RolesUser.user_has_role?(self, role)
-          #   end
-          # end
-          
-          def has_permission?(permission)
-            @has_permission ||= {}
-            @has_permission[permission] ||= begin
-              return true if self.respond_to?(:admin?) && self.admin?
-              !permission.roles_users.where(user_id: self.id).count.zero?
-            end
-          end
-          
-          def has_permission_with_name?(permission_name)
-            @has_permission_with_name ||= {}
-            @has_permission_with_name[permission_name] ||= begin
-              return true if self.respond_to?(:admin?) && self.admin?
-              has_permission?(Permission.find_or_create_by_name(permission_name))
-            end
+      def has_permission?(permission)
+        @has_permission ||= {}
+        @has_permission[permission] ||= begin
+          if self.respond_to?(:admin?) && self.admin?
+            true
+          else
+            !permission.roles_users.where(user_id: self.id).count.zero?
           end
         end
-
+      end
+      
+      def has_permission_with_name?(permission_name)
+        @has_permission_with_name ||= {}
+        @has_permission_with_name[permission_name] ||= begin
+          if self.respond_to?(:admin?) && self.admin?
+            true
+          else
+            has_permission?(Permission.find_or_create_by_name(permission_name))
+          end
+        end
       end
 
       def method_missing(method_name, *args, &block)
