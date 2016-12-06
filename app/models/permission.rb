597c604ae3435df4b20d69bc3dd589612072092a
Ben Zheng <ben@ekohe.com>
2016-12-05 16:10:19 -0800

    MISC #59926 Upgrade MRLDB
    - Delete attr_accessible in Permission model

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,6 +1,4 @@
 class Permission < ActiveRecord::Base
-  attr_accessible :name, :description, :roles
-  
   has_many :permissions_roles, dependent: :destroy
   has_many :roles, through: :permissions_roles
   has_many :roles_users, through: :roles
