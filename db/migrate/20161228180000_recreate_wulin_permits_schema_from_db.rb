d021c9832d4bab0a9c3222f372d8cc935e105e3f
Maxime Guilbot <maxime@ekohe.com>
2017-02-02 17:29:51 +0900

    MISC - Modify migration to be non destructive

--- db/migrate/20161228180000_recreate_wulin_permits_schema_from_db.rb
+++ db/migrate/20161228180000_recreate_wulin_permits_schema_from_db.rb
@@ -1,28 +1,28 @@
 class RecreateWulinPermitsSchemaFromDb < ActiveRecord::Migration[5.0]
   def change
-    create_table "roles_users", force: :cascade do |t|
+    create_table "roles_users" do |t|
       t.integer  "user_id"
       t.integer  "role_id"
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
-    end
+    end unless table_exists?(:roles_users)
 
-    create_table "permissions", force: :cascade do |t|
+    create_table "permissions" do |t|
       t.string   "name"
       t.datetime "created_at",              null: false
       t.datetime "updated_at",              null: false
       t.string   "description"
-    end
+    end unless table_exists?(:permissions)
 
-    create_table "permissions_roles", force: :cascade do |t|
+    create_table "permissions_roles" do |t|
       t.integer "permission_id"
       t.integer "role_id"
-    end
+    end unless table_exists?(:permissions_roles)
 
-    create_table "roles", force: :cascade do |t|
+    create_table "roles" do |t|
       t.string   "name"
       t.datetime "created_at",             null: false
       t.datetime "updated_at",             null: false
-    end
+    end unless table_exists?(:roles)
   end
 end
