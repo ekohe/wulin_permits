7f5c0300312ed1ef1686e2378bdddaf5fba68f35
Ben Zheng <ben@ekohe.com>
2017-11-20 17:49:15 -0800

    FEATURE - Use material-icon for toolbar items: User Roles / RolesUsers

--- app/grids/roles_user_grid.rb
+++ app/grids/roles_user_grid.rb
@@ -7,10 +7,10 @@ class RolesUserGrid < WulinMaster::Grid
   column :user, source: 'email', only: [:MasterRoleDetailUserScreen]
 
   # actions for master sad detail role grid
-  action :add_detail, model: 'role', screen: 'AddRoleForUserScreen', title: 'Add Roles', only: [:MasterUserDetailRoleScreen]
+  action :add_detail, model: 'role', screen: 'AddRoleForUserScreen', title: 'Roles', only: [:MasterUserDetailRoleScreen]
 
   # actions for master service detail sad grid
-  action :add_detail, model: 'user', screen: 'AddUserForRoleScreen', title: 'Add Users', only: [:MasterRoleDetailUserScreen]
+  action :add_detail, model: 'user', screen: 'AddUserForRoleScreen', title: 'Users', only: [:MasterRoleDetailUserScreen]
 
   # Common actions
   action :export
--- app/screens/master_user_detail_role_screen.rb
+++ app/screens/master_user_detail_role_screen.rb
@@ -3,8 +3,8 @@ class MasterUserDetailRoleScreen < WulinMaster::Screen
 
   path '/roles_users'
 
-  grid UserGrid, width: '47%', height: '100%', css: 'position: absolute; bottom: 0; left: 0;'
-  grid RolesUserGrid, width: '53%', height: '100%', css: 'position: absolute; bottom: 0; right: 0;', title: 'Roles with User', master_grid: 'UserGrid', eager_loading: false
+  grid UserGrid, width: '50%', height: '100%', css: 'position: absolute; bottom: 0; left: 0;'
+  grid RolesUserGrid, width: '50%', height: '100%', css: 'position: absolute; bottom: 0; right: 0;', title: 'Roles with User', master_grid: 'UserGrid', eager_loading: false
 
   def authorized?(user)
     user.admin?
