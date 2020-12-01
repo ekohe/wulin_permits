46d92de4e9653d3357114f23b54556fbcbbf0bc6
mel <mel@ekohe.com>
2020-12-01 17:50:53 +0800

    Feature #7 - Fix can't filter or sort in the 'attach role' modal

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,16 +1,33 @@
+# frozen_string_literal: true
+
 class RolesController < WulinMaster::ScreenController
   controller_for_screen RoleScreen
-  
-  add_callback :query_ready, :filter_for_user
-  
-  # protected
-  
-  def filter_for_user
-    user_filter_params = params[:filters].to_a.find{|x| x.values.include?("user_id") and x.values.include?("exclude")}
-    return if user_filter_params.blank?
 
-    role_ids = RolesUser.where(:user_id => user_filter_params[:value]).pluck(:role_id).uniq
-    
+  add_callback :query_ready, :filter_for_user
+  add_callback :query_ready, :filter_role
+
+  private
+
+  def filter_role
+    filter = params[:filters].to_a.find { |x| x.value?("name") }
+    return if filter.blank? || filter[:value].blank?
+
+    @query ||= grid.model
+
+    case filter[:operator]
+    when "equals"
+      @query = @query.where("name ~* ?", filter[:value])
+    when "exclude"
+      @query = @query.where("name <> ?", filter[:value])
+    end
+  end
+
+  def filter_for_user
+    filter = params[:filters].to_a.find { |x| x.value?("user_id") && x.value?("exclude") }
+    return if filter.blank?
+
+    role_ids = RolesUser.where(user_id: filter[:value]).pluck(:role_id).uniq
+
     @query = grid.model
     @query = grid.model.where("id NOT IN (?)", role_ids) if role_ids.present?
   end
