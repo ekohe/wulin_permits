84834d41300a3124521c80ffc728a64af48cef1a
Wang <sarah@ekohe.com>
2021-06-04 13:57:30 +0800

    Rename MasterRolePermissionScreen back to RoleScreen

--- app/grids/permissions_role_grid.rb
+++ app/grids/permissions_role_grid.rb
@@ -5,14 +5,14 @@ class PermissionsRoleGrid < WulinMaster::Grid
 
   path "/permissions_roles"
 
-  column :permission, editable: false, only: [:MasterRolePermissionScreen]
-  column :description, sql_expression: "#{Permission.table_name}.description", only: [:MasterRolePermissionScreen]
+  column :permission, editable: false, only: [:RoleScreen]
+  column :description, sql_expression: "#{Permission.table_name}.description", only: [:RoleScreen]
 
   column :name, through: :role, only: [:MasterPermissionRoleScreen]
 
-  # actions for MasterRolePermissionScreen
-  action :add_detail, icon: :add_circle_outline, model: 'permission', screen: 'AddPermissionToRoleScreen', title: 'Attach Permissions', only: [:MasterRolePermissionScreen]
-  action :delete, icon: :remove_circle_outline, title: 'Remove Permissions', only: [:MasterRolePermissionScreen]
+  # actions for RoleScreen
+  action :add_detail, icon: :add_circle_outline, model: 'permission', screen: 'AddPermissionToRoleScreen', title: 'Attach Permissions', only: [:RoleScreen]
+  action :delete, icon: :remove_circle_outline, title: 'Remove Permissions', only: [:RoleScreen]
   # actions for MasterPermissionRoleScreen
   action :add_detail, icon: :add_circle_outline, model: 'role', screen: 'AddRoleToPermissionScreen', title: 'Roles', only: [:MasterPermissionRoleScreen]
   action :delete, icon: :remove_circle_outline, title: 'Remove Roles', only: [:MasterPermissionRoleScreen]
--- app/screens/role_screen.rb
+++ app/screens/role_screen.rb
@@ -1,15 +1,10 @@
-# frozen_string_literal: true
-
 class RoleScreen < WulinMaster::Screen
   title 'Roles Permissions'
 
   path '/roles'
 
-  grid RoleGrid, height: '50%'
-
-  grid PermissionsRoleGrid, height: '50%', width: '45%', title: 'Existing permissions', include_of: "RoleGrid", eager_loading: false
-  panel WulinMaster::InclusionExclusionPanel, height: '50%', width: '10%', inclusion_grid: 'PermissionsRoleGrid', exclusion_grid: 'PermissionGrid'
-  grid PermissionGrid, height: '50%', width: '45%', title: 'Available permissions', exclude_of: "RoleGrid", eager_loading: false
+  grid RoleGrid, height: '100%', width: '50%', title: 'Role'
+  grid PermissionsRoleGrid, height: '100%', width: '50%', title: 'Existing permissions', master_grid: 'RoleGrid', eager_loading: false
 
   def authorized?(user)
     user&.admin?
