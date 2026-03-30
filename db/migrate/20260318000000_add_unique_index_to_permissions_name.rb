6ef0d7c69e30a6cb9ffabb31fa603d949dc84079
Mel Cao <mel@ekohe.com>
2026-03-30 17:29:53 +0800

    changed ActiveRecord::Migration[7.1] to 6.1

--- db/migrate/20260318000000_add_unique_index_to_permissions_name.rb
+++ db/migrate/20260318000000_add_unique_index_to_permissions_name.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexToPermissionsName < ActiveRecord::Migration[7.1]
+class AddUniqueIndexToPermissionsName < ActiveRecord::Migration[6.1]
   def change
     add_index :permissions, :name, unique: true, name: "uniq_permissions_name", if_not_exists: true
   end
